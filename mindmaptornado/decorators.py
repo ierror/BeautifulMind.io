c43358ab537a3250ab9e811fbaa7fb10d0a14705
Börni <boerni@gmail.com>
2012-05-29 23:47:52 +0200

    [CHG] make error msg more clear

--- mindmaptornado/decorators.py
+++ mindmaptornado/decorators.py
@@ -8,7 +8,7 @@ def check_for_map_pk_data(method):
         try:
             args[1]['map_pk'] = int(args[1]['map_pk'])
         except (KeyError, IndexError, ValueError):
-            raise HTTPException(log_message='check_for_map_pk_data decorated methods need an int map_pk data key')
+            raise HTTPException(log_message='check_for_map_pk_data decorated methods need an integer map_pk data key')
 
         method(*args, **kwargs)
     return wrapped
