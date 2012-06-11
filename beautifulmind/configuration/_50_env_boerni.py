8342ab83edbfb14a85571f41d5e244a2bd610734
Börni <boerni@gmail.com>
2012-06-11 22:17:45 +0200

    [FIX] make fork me ribbon clickable

--- beautifulmind/configuration/_50_env_boerni.py
+++ beautifulmind/configuration/_50_env_boerni.py
@@ -15,11 +15,11 @@ DATABASES = {
 
 MEDIA_URL = 'http://192.168.100.200:8000/assets/'
 
-if not settings.DEBUG:
-    STATIC_URL = 'http://192.168.100.200:8000/assets/static/'
+#if not settings.DEBUG:
+#    STATIC_URL = 'http://192.168.100.200:8000/assets/static/'
 
 COMPRESS_ENABLED = False
-DEBUG = False
+DEBUG = True
 
 MINDMAPTORNADO_BIND_PORT = 1234
 MINDMAPTORNADO_SERVER = 'http://192.168.100.200:%d/ws' % (MINDMAPTORNADO_BIND_PORT)
