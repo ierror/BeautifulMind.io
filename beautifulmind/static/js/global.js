e8f607150981f89ae8e599d0c49dd2458b2103d7
Börni <boerni@gmail.com>
2012-06-12 23:00:17 +0200

    [CLEANUP] move js

--- beautifulmind/static/js/global.js
+++ beautifulmind/static/js/global.js
@@ -28,4 +28,17 @@ $(document).ready(function(){
             }
         }
     });
+
+    $(document).ready(function(){
+        $('#mindmap-new').on('initiated', function() {
+            // write component pos based on window size
+            $('#id_root_component_pos_left', this).attr('value', parseInt($(window).width() / 2 - 50));
+            $('#id_root_component_pos_top', this).attr('value', parseInt($(window).height() / 2 - 9));
+
+            // remove focus from selected component
+            $('.component-selected').each(function(){
+                $(this).data('mindmapMapComponent').deselect();
+            })
+        });
+    });
 });
