23d3274766c42592d774e2deb48c432761a54b88
Börni <boerni@gmail.com>
2012-06-12 23:00:57 +0200

    [CLEANUP]

--- beautifulmind/static/js/global.js
+++ beautifulmind/static/js/global.js
@@ -29,16 +29,14 @@ $(document).ready(function(){
         }
     });
 
-    $(document).ready(function(){
-        $('#mindmap-new').on('initiated', function() {
-            // write component pos based on window size
-            $('#id_root_component_pos_left', this).attr('value', parseInt($(window).width() / 2 - 50));
-            $('#id_root_component_pos_top', this).attr('value', parseInt($(window).height() / 2 - 9));
+    $('#mindmap-new').on('initiated', function() {
+        // write component pos based on window size
+        $('#id_root_component_pos_left', this).attr('value', parseInt($(window).width() / 2 - 50));
+        $('#id_root_component_pos_top', this).attr('value', parseInt($(window).height() / 2 - 9));
 
-            // remove focus from selected component
-            $('.component-selected').each(function(){
-                $(this).data('mindmapMapComponent').deselect();
-            })
-        });
+        // remove focus from selected component
+        $('.component-selected').each(function(){
+            $(this).data('mindmapMapComponent').deselect();
+        })
     });
 });
