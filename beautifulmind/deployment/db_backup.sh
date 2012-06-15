3b1e10f702eff92b9465553e03a6d5a19ca50c12
Börni <boerni@gmail.com>
2012-06-15 16:26:47 +0200

    [CHG] contributors list / links

--- beautifulmind/deployment/db_backup.sh
+++ beautifulmind/deployment/db_backup.sh
@@ -2,4 +2,5 @@
 DIR=/opt/beautifulmind/backup/
 FILENAME=db_$(date +"%Y%m%d%H%M%S").gz
 FILENAMETMP=$FILENAME-tmp;
-mkdir -p $DIR && pg_dump -U postgres beautifulmind | gzip -c > $DIRFILENAMETMP && mv FILENAMETMP FILENAME
+mkdir -p $DIR && cd $DIR && pg_dump -U postgres beautifulmind | gzip -c > $FILENAMETMP 
+mv $FILENAMETMP $FILENAME
