ef810f12c55825e5b46702c8ea5f4fae0a5692c7
Börni <boerni@gmail.com>
2012-06-26 23:49:12 +0200

    [CHG] disable CsrfViewMiddleware

--- beautifulmind/settings.py
+++ beautifulmind/settings.py
@@ -113,7 +113,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.transaction.TransactionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     #'django.contrib.messages.middleware.MessageMiddleware',
 )
